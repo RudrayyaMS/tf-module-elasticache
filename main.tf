4817c8c89c5cd644f27f3c8a72a1cc6fac63aaa8
Rudra <Rudrayyaswamims@gmail.com>
2023-06-27 00:32:39 +0530

    updating tf-module-elasticache

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_elasticache_cluster" "elasticache" {
   num_cache_nodes      = var.num_cache_nodes
   engine_version       = var.engine_version
   port                 = 6379
-  subnet_group_name = "aws_elasticache_subnet_group.main.name"
+  subnet_group_name = aws_elasticache_subnet_group.main.name
 
   tags = merge(
     var.tags,
@@ -19,7 +19,7 @@ name       = "${var.env}-elasticache"
 subnet_ids = var.subnet_ids
 
 tags = merge(
-var.tags,
-{ Name = "${var.env}-subnet-group" }
-)
+  var.tags,
+  { Name = "${var.env}-subnet-group" }
+  )
 }
